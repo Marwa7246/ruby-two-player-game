9b0dbb9a0bc012d12a054692be32e03609e15d59
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-14 13:32:08 -0400

    Implement Player class

---
+++ Player.rb
@@ -0,0 +1,17 @@
+class Player
+  attr_reader :name
+  attr_accessor :lives
+
+  def initialize(name)
+    @name=name
+    @lives=3
+  end
+
+  def decrease_lives
+    @lives -=1
+  end
+  
+end
+
+player1=Player.new('player1')
+puts player1
